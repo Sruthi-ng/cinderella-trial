071b6c4b9e6a024a57f07ea2a51bf91b6c8b65fd
Sruthi-ng <sruthi.nagari93@gmail.com>
2025-04-12 13:15:45 +0000

    formatted hello.py

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ params = {
     "longitude": LONGITUDE,
     "hourly": "temperature_2m,weathercode",  # Specify the data you want
     "current_weather": True,  # Include current weather data
-    "timezone": "auto"  # Automatically adjust timezone
+    "timezone": "auto",  # Automatically adjust timezone
 }
 
 # Make the API request
@@ -23,7 +23,7 @@ if response.status_code == 200:
     # Parse the JSON response
     weather_data = response.json()
     print(weather_data.keys())  # Print the keys of the JSON response
-    data=json.dumps(weather_data, indent=4)
+    data = json.dumps(weather_data, indent=4)
     print(type(data))
     print(data)  # Pretty-print the JSON data
 else:
